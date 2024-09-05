71c31c9e94b89b4cb81fd468de9d298bdc0e9cad
merealtea <1349621180@qq.com>
2024-09-05 21:06:13 +0800

    Complete preprocess for image

--- scripts/hycan/hycan_launch.sh
+++ scripts/hycan/hycan_launch.sh
@@ -1,5 +1,6 @@
 source ../../devel/setup.bash
-chmod +x ../../src/hycan/communication/src/socket_hycan_client.py
+chmod +x ../../src/hycan/communication/script/socket_hycan_client.py
+chmod +x ../../src/hycan/detection/src/detector.py
 echo "Run..."
 gnome-terminal --window --title "hycan_localization" -e 'bash -c "roslaunch --wait hycan_localization localization.launch ; exec bash;"'\
 gnome-terminal --window --title "hycan_comm" -e 'bash -c "roslaunch --wait hycan_comm hycan_comm.launch ; exec bash;"' /
